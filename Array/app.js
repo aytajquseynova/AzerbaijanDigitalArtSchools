e8211834b1f78af4540f2c499a450c3011fe5415
aytajquseynova <you@example.com>
2023-08-30 13:28:20 +0400

    array and object destruction

---
+++ Array/app.js
@@ -0,0 +1,80 @@
+// const obj = {
+//     firstName : 'Aytac',
+//     secondName :' Huseynova',
+//     getFullName : function(){
+//         return this.firstName + " "+ this.secondName;
+//     },
+
+// };
+
+// console.log(obj ["getFullName"]());
+
+// const obj = {
+//     title_az : "Html dersi",
+//     title_en: "Lesson Html",
+// };
+
+// let key = "title_";
+// key += "en";
+
+// console.log(obj[key]);
+
+// const arr = [ 1, 2 , 3, 4];
+
+// let calc = 0;
+
+// for(let i= 0; i<arr.length, i++){
+//     calc+= arr[i];
+// }
+// console.log(calc);
+
+// const [ a, b, c= 5] = [1, 2];
+// console.log(a, b, c);
+
+// Interview suali
+// const arr =[1, 2];
+// let a = arr[0];
+// let b = arr[1];
+
+// let temp = a; 
+// a = b;
+// b= temp;
+// // a = 2, b= 1
+// console.log(a, b);
+
+// let [a, b]= [1, 2];
+// [b, a] = [a, b];
+// console.log([a, b]);
+
+// let arr = [1, 2, [-1, -2, -3], 4];
+
+// let[a, b, [v1, v2, v3], c] = arr;
+
+// console.log(a, b, v1, v2, v3, c);
+
+// let {a:first, b:myNum, c: third} ={a:1, b:2 , c:3};
+
+// console.log(first, myNum, third);
+
+// let arr= [1, 2];
+// let newArr= [...arr, 3, 4];
+// console.log(newArr);
+
+let arr = [1, 3, 4];
+
+// let newArr =[...arr];
+// console.log(arr, newArr);
+
+// let newArr = [...arr];
+// newArr.push(2);
+// console.log(arr, newArr);
+
+// Concat method
+
+// let arr1 =[1, 2, 3];
+// let arr2 =[ 4, 5, 6];
+
+// let newArr = arr1.concat(arr2);
+
+// console.log(newArr);
+
