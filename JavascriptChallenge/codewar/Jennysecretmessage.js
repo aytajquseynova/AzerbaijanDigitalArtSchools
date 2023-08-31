d71314d6e273616759075f1311781abaa654ed9f
aytajquseynova <you@example.com>
2023-08-31 15:49:15 +0400

    tasks

---
+++ JavascriptChallenge/codewar/Jennysecretmessage.js
@@ -0,0 +1,8 @@
+function greet(name) {
+    if (name === "Johnny") {
+        return "Hello, my love!";
+    } else if (name !== "Johnny") {
+        return "Hello, " + name + "!";
+    }
+}
+greet("Johnny");
---
+++ JavascriptChallenge/codewar/billboard.js
@@ -0,0 +1,14 @@
+function billboard(name, price = 30) {
+   let result = 0
+
+   for(let i = 0; i < name.length; i++) {
+    result +=price
+   }
+
+   return result
+
+}
+console.log(billboard("Hadufuns John", 20))
+
+
+
