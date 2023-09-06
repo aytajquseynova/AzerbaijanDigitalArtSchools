622fb14d4a1542fc7d4b14076f5c703fd2b2d7ca
aytajquseynova <you@example.com>
2023-09-06 12:47:17 +0400

    map and set

--- JavascriptChallenge/tasks-05-09/task10.js
+++ JavascriptChallenge/tasks-05-09/task10.js
@@ -1,6 +1,15 @@
 // 1 deyisen string kimi . hemin string ad soyad ata ad  boyuk herfleri kicik 
-let user = "Huseynova Aytac Qedir";
+let user = "Huseynova Aytac Qedir Eli Aytac";
+let txt = user.split(" ");
+// console.log(user.slice(0,1).toLowerCase()
+// + "."+ user.slice(10,11).toLowerCase()
+// + "." + user.slice(16, 17).toLowerCase());
 
-console.log(user.slice(0,1).toLowerCase()
-+ "."+ user.slice(10,11).toLowerCase()
-+ "." + user.slice(16, 17).toLowerCase());
+
+// console.log(txt[0][0].toLowerCase(), txt[1][0].toLowerCase(), txt[2][0].toLowerCase())
+
+let letters =[];
+for (let i = 0; i< txt.length ; i++){
+    letters+= txt[i][0].toLowerCase();
+}
+console.log(letters);
---
+++ jsMapandSet/app.js
@@ -0,0 +1,33 @@
+// const myArray = [1, 2, 3, 4, 5, 5, 1 ,2];
+// const mySet = new Set(myArray);
+// // const uniqeueArray = [...mySet];
+// // console.log(uniqeueArray);
+
+// // mySet.add({myChannel : "JAVASCRIPT"});
+
+// // mySet.delete(4);
+
+// // mySet.clear();
+
+
+// console.log(mySet);
+
+// const myMap = new Map([['name', 'John'], ['surname', 'Doe']]);
+
+// console.log(myMap);
+
+
+// const a ={};
+// const b = {};
+// const c ={}
+// // myObject[a] ="a"; //sehv
+// // myObject[b] = "b"; // sehv
+// const myMap = new Map([[a,"a"], [b,"b"], [c,'c']])
+
+// const myMap = new Map ([[{}, "a"], [{}, "ba"], [{}, "ca"]]);
+// myMap.set({}, "da"); yeni object elave edir
+const a ={};
+const myMap = new Map([[1,'one'], [a,'a']]);
+// myMap.delete(a);
+// console.log(myMap.size);
+// console.log(myMap.has(a));
