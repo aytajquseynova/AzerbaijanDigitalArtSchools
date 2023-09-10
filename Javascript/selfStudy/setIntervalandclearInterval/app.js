3508f6540907a2a7d93d5dd8ee99d45ce11e17eb
aytajquseynova <you@example.com>
2023-09-10 15:34:59 +0400

    edits

--- Javascript/selfStudy/setIntervalandclearInterval/app.js
+++ Javascript/selfStudy/setIntervalandclearInterval/app.js
@@ -1,25 +1,61 @@
+//Create Elements
 let body = document.querySelector("body");
-let p = document.createElement('p');
-body.append(p);
+let div = document.createElement('div');
+let h1 = document.createElement('h1');
+let buttonDiv = document.createElement('div');
+let generalBox = document.createElement('div');
+let button1Div = document.createElement('div');
+let button2Div = document.createElement('div');
 
-p.style.fontSize = "32px";
-p.style.padding = "0px 32px";
-p.className = "count";
+//Appends
+generalBox.append(buttonDiv);
+generalBox.append(div);
+body.append(generalBox);
+div.append(h1);
+buttonDiv.append(button1Div);
+buttonDiv.append(button2Div);
 
+//Styles
+h1.style.fontSize = "32px";
+h1.className = "count";
+
+generalBox.style.display = "flex";
+generalBox.style.flexDirection ="column-reverse";
+generalBox.style.gap = "20px";
+
+div.style.background = "#white";
+div.style.color = "seagreen";
+div.style.border ="2px solid seagreen";
+div.style.margin ="0 auto";
+div.style.width = "500px";
+div.style.padding = "32px";
+div.style.textAlign= 'center';
+
+buttonDiv.style.width = "300px";
+buttonDiv.style.margin = "0 auto";
+buttonDiv.style.display ="flex";
+buttonDiv.style.gap ="30px";
+buttonDiv.style.justifyContent="center";
+
+//Button1 styles
 let button1 = document.createElement("button");
-body.append(button1);
+button1Div.append(button1);
 button1.textContent = "Start";
 button1.className = "start";
 button1.style.padding = "8px 32px";
-button1.style.width = "100px";
+button1.style.backgroundColor ="seagreen";
+button1.style.color = "bisque";
+button1.style.cursor="pointer";
 
+//Button2 styles
 let button2 = document.createElement("button");
-body.append(button2);
+button2Div.append(button2);
 button2.textContent = "Stop";
 button2.className = "stop";
 button2.style.padding = "8px 32px";
-button2.style.marginLeft = "5px";
-button2.style.width = "100px";
+button2.style.backgroundColor = "seagreen";
+button2.style.color = "bisque";
+button2.style.cursor = "pointer";
 
 
 // setInterval(function, ms)
@@ -29,7 +65,7 @@ let time = document.querySelector(".count");
 // console.log(time);
 let count = 30,
     clear;
-p.textContent = count;
+h1.textContent = count;
 document.querySelector('.start').addEventListener('click', () => {
     clear = setInterval(fromEndtoZero, 1000);
 });
@@ -37,8 +73,9 @@ document.querySelector('.start').addEventListener('click', () => {
 document.querySelector('.stop').addEventListener('click', () => {
     clearInterval(clear);
 })
-document.querySelector("")
 
+
+//0-30
 function fromZeroToEnd() {
     if (count < 30) {
         count++
@@ -46,6 +83,7 @@ function fromZeroToEnd() {
     money.textContent = count;
 }
 
+// 30-0
 function fromEndtoZero() {
     if (count > 0) {
         count--;
