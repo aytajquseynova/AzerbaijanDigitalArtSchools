3aa3714a7b5310a3a1849ab20c1d902c7c5f5445
aytajquseynova <you@example.com>
2023-09-10 14:41:46 +0400

    timer

--- Javascript/selfStudy/setIntervalandclearInterval/app.js
+++ Javascript/selfStudy/setIntervalandclearInterval/app.js
@@ -1,44 +1,54 @@
 let body = document.querySelector("body");
 let p = document.createElement('p');
 body.append(p);
-p.textContent ="0";
-p.style.fontSize ="32px";
-p.style.padding ="0px 32px";
-p.className ="count";
+
+p.style.fontSize = "32px";
+p.style.padding = "0px 32px";
+p.className = "count";
 
 let button1 = document.createElement("button");
 body.append(button1);
 button1.textContent = "Start";
-button1.className ="start";
+button1.className = "start";
 button1.style.padding = "8px 32px";
 button1.style.width = "100px";
 
 let button2 = document.createElement("button");
 body.append(button2);
 button2.textContent = "Stop";
-button2.className ="stop";
+button2.className = "stop";
 button2.style.padding = "8px 32px";
-button2.style.marginLeft ="5px";
-button2.style.width= "100px";
+button2.style.marginLeft = "5px";
+button2.style.width = "100px";
 
 
 // setInterval(function, ms)
 // 1000 ms = 1sec
 
-let money = document.querySelector(".count");
-// console.log(money);
-let count = 0,
-clear;
-document.querySelector('.start').addEventListener('click',() =>{
-    clear =setInterval(ValueCount, 1000); 
+let time = document.querySelector(".count");
+// console.log(time);
+let count = 30,
+    clear;
+p.textContent = count;
+document.querySelector('.start').addEventListener('click', () => {
+    clear = setInterval(fromEndtoZero, 1000);
 });
 
-document.querySelector('.stop').addEventListener('click', () =>{
+document.querySelector('.stop').addEventListener('click', () => {
     clearInterval(clear);
-}
-)
+})
 document.querySelector("")
-function ValueCount(){
-    count++;
+
+function fromZeroToEnd() {
+    if (count < 30) {
+        count++
+    } else return;
     money.textContent = count;
+}
+
+function fromEndtoZero() {
+    if (count > 0) {
+        count--;
+    } else return;
+    time.textContent = count;
 }
