5bfd41d9fd58ec29af8fd03d43dc798c1b21afc0
aytajquseynova <you@example.com>
2023-09-11 14:33:48 +0400

    tarih islemleri

--- Javascript/selfStudy/Boolean-Operator-Date/app.js
+++ Javascript/selfStudy/Boolean-Operator-Date/app.js
@@ -37,4 +37,57 @@
 
 // window prompt method
 
+//confirm 
+
+//Date object
+
+// getMonth 0-11 arasi , getTime (millisecond since Jan 1, 1970)
+//Getday 0-6 hefte gunu
+
+let now = new Date();
+// console.log(now);
+
+// console.log(now.getFullYear()); // hal hazirdaki il
+// console.log(now.getTime());
+// console.log(now.getDay()); //hefte gunu
+
+// console.log(Date.now()); // beraberdi getTime methoduna
+
+// let year = now.getFullYear();
+// let month = now.getMonth();
+// let day = now.getDay();
+// let hour = now.getHours();
+// let minute = now.getMinutes();
+// let second = now.getSeconds();
+
+// let months =['Yanvar',
+//             'Fevral',
+//             'Mart',
+//             'Aprel',
+//             'May',
+//             'Iyun',
+//             'Iyul',
+//             'Avqust',
+//             'Sentyabr',
+//             'Oktyabr',
+//             'Noyabr',
+//             'Dekabr'
+//             ]
+// let days = [
+//     'Bazar Ertesi',
+//     'Cersenbe axsami',
+//     'Cersenbe',
+//     'Cume axsami',
+//     'Cume', 
+//     'Senbe',
+//     'Bazar'
+// ]            
+// // console.log(months[0]);
+// // console.log(days[1]);
+
+// let humanReadableDate = `${day} ${months[month]} ${year}, ${days[day]}, ${hour}: ${minute}:${second}}`;
+// console.log(humanReadableDate);
+
+
+
 
