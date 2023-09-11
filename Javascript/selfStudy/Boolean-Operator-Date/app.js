bf24c4a042157295c2f1605cc729ca3f48cda699
aytajquseynova <you@example.com>
2023-09-11 14:12:10 +0400

    boolean

---
+++ Javascript/selfStudy/Boolean-Operator-Date/app.js
@@ -0,0 +1,40 @@
+//Boolean
+
+// Truthy values  
+// All numbers - positive and neagtive are truthy except zero
+// All strings are truthy except an empty string("")
+// The boolean true
+
+//Falsy values 
+// 0, 0n , null, undefined, Nan, the boolean false,"", '' , `` , empty string
+
+// Operators
+
+// === hem deyeri hem data tipinin beraberliyini yoxlayir
+// == ise tekce deyeri yoxlayir
+
+//Logical operators
+// true&& true => true
+// true &&false => false
+// false&& false => false
+
+//Ternary Operator
+
+// let isRaining = true
+// isRaining
+// ? console.log('You need a rain coat.')
+// : console.log('No need for a rain coat.')
+
+// // console.log(isRaining)
+
+// 5>4 ? console.log('beli')
+// : console.log('xeyr')
+
+//Window methods 
+
+//alert method
+// alert('welcome') -cox isletmeyin
+
+// window prompt method
+
+
---
+++ Javascript/selfStudy/DataTypes/app.js
@@ -0,0 +1,30 @@
+// String Methods
+// 54: 56 length
+// Accessing characters in String
+// toUpperCase()
+// toLowerCase()
+// substr()
+// substring()
+// split()
+// trim()
+// includes()
+// replace()
+// charAt()
+// charCodeAt()
+// indexOf()
+// lastIndexOf()
+// concat()
+// startsWith()
+// endsWith()
+// search()
+// match
+
+// let string = 'Love is the most powerful feeling in the world'
+
+// console.log(string.endsWith('world'));
+// console.log(string.startsWith("is"));
+
+let string = "I am learning Js";
+console.log(string.search('Js')); //14
+
+
