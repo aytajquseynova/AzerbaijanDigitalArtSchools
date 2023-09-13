edd493b061358e3841c21270ae6a5d62efcb0310
aytajquseynova <you@example.com>
2023-09-13 12:41:53 +0400

    functions

---
+++ Javascript/selfStudy/Function/app.js
@@ -0,0 +1,68 @@
+// a function can be declared or created in couple of ways
+
+// declaration function
+// expression function
+// anonymous function
+// arrow function
+
+// function declaration
+
+// function functionName(){
+//     // code goes  here
+// }
+// functionName() //calling function by its name and with parentheses
+
+// function sayHello(){
+//     console.log('Salam');
+// }
+// sayHello()
+
+// fucntion without a parametr and return
+
+// function square(){
+//     let num = 2
+//     let sq = num *num
+//     console.log(sq)
+// }
+// square()
+
+// function with parametr
+
+// function functionName(para1){
+//     // code goes here
+// }
+// functionName(para1)
+
+// function functionDevide(num1, num2){
+//     console.log(num1 / num2);
+// }
+// functionDevide(16, 8);
+
+// function functionDevide(num1, num2){
+//     let total = num1 / num2;
+//     return total
+// }
+// let total = functionDevide(16, 8)
+// console.log(total);
+
+// anonymous - adsiz funksiya
+// deyiskene menimsedilmis funksiya
+
+// let sayHello = function(name){
+//     return `Hello ${name}`;
+// }
+// console.log(sayHello('Aytac'))
+
+// anonim ve expression funksiya eyni seydi
+
+// arrow function
+// normal funksiya
+// function hello(name){
+    // return `hello ${name}`
+// }
+// oxlu funksiya
+// let hello = (name, surname)=>{
+//     return `hello ${name} ${surname}, how are you? `
+// }
+// console.log(hello('Aytac', 'Huseynova'))
+
