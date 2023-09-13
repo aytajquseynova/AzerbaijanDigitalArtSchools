a1b9d1ef5e8067db24ffde265305f513002b29f5
aytajquseynova <you@example.com>
2023-09-13 18:44:25 +0400

    tasks

---
+++ Javascript/TasksforJs/task-13-09/app.js
@@ -0,0 +1,4 @@
+// let container = document.getElementById('footer-paragraph').parentElement.parentElement
+// let header = container.children[0];
+// let basket = header.lastElementChild;
+// console.log(basket); ..// basket tapdi
