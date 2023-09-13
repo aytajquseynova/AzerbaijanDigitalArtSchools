ca2658ab77f19b43377edcdcea630231d211da2d
aytajquseynova <you@example.com>
2023-09-13 11:40:32 +0400

    loops

---
+++ Javascript/selfStudy/Loops/app.js
@@ -0,0 +1,66 @@
+// for loop
+// for (i =1; i<=10; i++){
+//     console.log(`${i} * ${i} = ${i * i}`);
+// } //backtickle de yazmaq olur
+
+// const names = ['Aytac', 'Sabina', 'Nargiz', 'Guler', 'Aysel']
+// for(let i=0; i < names.length ; i++){
+//     console.log(names[i]);
+// }
+
+// adding all elements in array
+
+// let numbers = [1, 2, 3 ,4]
+// let sum = 0;
+// for(let i=0; i < numbers.length ; i++){
+//     sum+= numbers[i]; //sum = sum+ numbers[i]
+// }
+// console.log(sum);
+
+// creating a new array based on existing array
+
+// let numbers = [1, 2, 3, 4, 5];
+// let newNumbers = []
+// let sum = 0;
+// for(let i= 0; i< numbers.length; i++){
+//     newNumbers.push(numbers[i] **2);
+// }
+// console.log(newNumbers);
+
+// let countries = ['Azerbaijan', 'Turkey', 'Germany']
+// let newArray = [];
+// for(i=0; i< countries.length; i++){
+//     newArray.push(countries[i].toUpperCase());
+// }
+// newArray.push('Canada');
+// console.log(newArray);
+
+// while loop //sert odenmese ise dusmur
+
+// let i =0 
+// while(i <=5 ){
+//     console.log(i)
+//     i++
+// }
+
+// do while //sert odendi odenmedi 1ci ise salir serti 1 defe de olsa
+// let a =0;   
+// do {
+//     console.log(i);
+//     i++;
+// } while(a <=5)
+
+// for of loop
+
+// for(const element of arr){
+//     //code goes here
+// }
+
+// let countries = ['Germany','Azerbaijan'];
+// let newArray =[];
+
+// for(country of countries){
+//     newArray.push(country)
+// }
+// console.log(newArray);
+
