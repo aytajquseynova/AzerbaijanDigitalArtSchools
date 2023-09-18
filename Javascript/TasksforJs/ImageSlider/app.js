9d9a996c44b047a4ec4260fb77242ddc5831b829
aytajquseynova <you@example.com>
2023-09-18 12:16:10 +0400

    slider img

---
+++ Javascript/TasksforJs/ImageSlider/app.js
@@ -0,0 +1,26 @@
+let slider_img = document.querySelector('.slider-img');
+let images = ['1.jpg', '2.jpg', '3.jpg'];
+let i = 0;
+
+let prevButton = document.getElementById('prev');
+prevButton.addEventListener('click', function () {
+    i--; 
+    if (i < 0) {
+        i = images.length - 1   ; 
+    }
+    setImg();
+});
+
+let nextButton = document.getElementById('next');
+nextButton.addEventListener('click', function () {
+    i++; 
+    if (i >= images.length) {
+        i = 0; 
+    }
+    setImg();
+});
+
+function setImg() {
+    return slider_img.setAttribute('src', "images/" + images[i]);
+
+}
