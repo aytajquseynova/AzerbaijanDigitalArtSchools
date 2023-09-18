b186bc5f8c6c7260870afb63d38138fd0c9072d5
aytajquseynova <you@example.com>
2023-09-18 11:41:51 +0400

    img slider

--- Javascript/TasksforJs/basicSlider/app.js
+++
@@ -1,7 +0,0 @@
-let body = document.querySelector('body');
-let arrowRight = document.getElementById("arrow-right");
-let arrowLeft = document.getElementById("arrow-left");
-
-arrowRight.addEventListener('click', function(){
- 
-})
